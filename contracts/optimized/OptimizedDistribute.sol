6f0bfc3eef48701d3825c0aa39aca03086f1c063
Francisco <jscriptcoder@gmail.com>
2023-02-08 10:43:01 +0100

    Solved Distribute. TODO is left

--- contracts/optimized/OptimizedDistribute.sol
+++ contracts/optimized/OptimizedDistribute.sol
@@ -22,14 +22,29 @@ contract OptimizedDistribute {
             "cannot distribute yet"
         );
 
-        uint256 amount;
-        unchecked {
-            amount = address(this).balance >> 2;
+        address _contributor1 = contributor1;
+        address _contributor2 = contributor2;
+        address _contributor3 = contributor3;
+        address _contributor4 = contributor4;
+
+        assembly {
+            // shr: logical shift right y by x bits. Two positions is like dividing by 4
+            // selfbalance: equivalent to balance(address()), but cheaper
+            // See https://docs.soliditylang.org/en/v0.8.15/yul.html
+            let amount := shr(2, selfbalance())
+
+            // pop: discard value
+            // call: call contract at specific address
+            // gas: gas still available to execution
+            // selfdestruct: end execution, destroy current contract and send funds to address
+            // See https://docs.soliditylang.org/en/v0.8.15/yul.html
+
+            pop(call(gas(), _contributor1, amount, 0, 0, 0, 0))
+            pop(call(gas(), _contributor2, amount, 0, 0, 0, 0))
+            pop(call(gas(), _contributor3, amount, 0, 0, 0, 0))
+
+            // TODO: better understand how the right amount is sent to 4
+            selfdestruct(_contributor4)
         }
-        
-        contributor1.transfer(amount);
-        contributor2.transfer(amount);
-        contributor3.transfer(amount);
-        contributor4.transfer(amount);
     }
 }
